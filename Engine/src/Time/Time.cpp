a2e69e55b090bbacad47f75c020a3a15392498cf
Zihad Joy <smz.joy73@gmail.com>
2026-03-20 00:47:19 +0600

    feat(time): add time module

---
+++ Engine/src/Time/Time.cpp
@@ -0,0 +1,104 @@
+#include "Time.hpp"
+
+zui::Clock::Clock()
+    : m_startTime(HighResClock::now()), m_deltaTime(0.0f), m_rawDeltaTime(0.0f), m_totalTime(0.0f), m_timeScale(1.0f),
+      m_isPaused(false), m_frameCount(0), m_fpsUpdateInterval(1.0f), m_fpsAccumulator(0.0f), m_fps(0.0f),
+      m_useFixedTimestep(false), m_fixedTimestep(1.0f / 60.0f), m_fixedTimeAccumulator(0.0f) {
+    m_currentTime = m_startTime;
+    m_lastTime    = m_startTime;
+}
+
+void zui::Clock::Tick() {
+    if (m_isPaused) {
+        m_deltaTime    = 0.0f;
+        m_rawDeltaTime = 0.0f;
+        return;
+    }
+
+    m_lastTime    = m_currentTime;
+    m_currentTime = HighResClock::now();
+
+    Duration dt    = m_currentTime - m_lastTime;
+    m_rawDeltaTime = dt.count();
+    m_deltaTime    = m_rawDeltaTime * m_timeScale;
+    m_totalTime += m_deltaTime;
+
+    m_frameCount++;
+    m_fpsAccumulator += m_deltaTime;
+    if (m_fpsAccumulator >= m_fpsUpdateInterval) {
+        m_fps            = static_cast<float>(m_frameCount) / m_fpsAccumulator;
+        m_fpsAccumulator = 0.0f;
+        m_frameCount     = 0;
+    }
+}
+
+void zui::Clock::Pause() {
+    m_isPaused = true;
+}
+void zui::Clock::Resume() {
+    m_isPaused = false;
+}
+void zui::Clock::TogglePause() {
+    m_isPaused = !m_isPaused;
+}
+bool zui::Clock::IsPaused() const {
+    return m_isPaused;
+}
+
+float zui::Clock::GetFps() const {
+    return m_fps;
+}
+float zui::Clock::GetDeltaTime() const {
+    return m_deltaTime;
+}
+double zui::Clock::GetElapsedTime() const {
+    return m_totalTime;
+};
+
+float zui::Clock::GetTimeScale() const {
+    return m_timeScale;
+}
+void zui::Clock::SetTimeScale(float timeScale) {
+    m_timeScale = (timeScale > 0.0f) ? timeScale : 0.0f;
+}
+
+void zui::Clock::Reset() {
+    m_startTime            = HighResClock::now();
+    m_currentTime          = m_startTime;
+    m_lastTime             = m_startTime;
+    m_deltaTime            = 0.0f;
+    m_totalTime            = 0.0f;
+    m_frameCount           = 0;
+    m_fpsAccumulator       = 0.0f;
+    m_fps                  = 0.0f;
+    m_fixedTimeAccumulator = 0.0f;
+}
+
+void zui::Clock::SetFpsUpdateInterval(float interval) {
+    m_fpsUpdateInterval = (interval > 0.0f) ? interval : 1.0f;
+}
+
+void zui::Profiler::Begin(const std::string &label) {
+    m_beginTimePoint.insert_or_assign(label, HighResClock::now());
+}
+
+void zui::Profiler::End(const std::string &label) {
+    m_endTimePoint.insert_or_assign(label, HighResClock::now());
+}
+
+double zui::Profiler::GetDuration(const std::string &label) {
+    std::unordered_map<std::string, TimePoint>::iterator it = m_beginTimePoint.find(label);
+    if (it == m_beginTimePoint.end()) {
+        return 0.0f;
+    }
+    TimePoint &beginTimePoint = it->second;
+
+    it = m_endTimePoint.find(label);
+    if (it == m_endTimePoint.end()) {
+        return 0.0f;
+    }
+
+    TimePoint &endTimePoint = it->second;
+
+    return Duration(endTimePoint - beginTimePoint).count();
+}
---
+++ Engine/src/Time/Time.hpp
@@ -0,0 +1,72 @@
+#pragma once
+#include <chrono>
+#include <string>
+#include <unordered_map>
+
+namespace zui {
+
+using HighResClock = std::chrono::high_resolution_clock;
+using TimePoint    = std::chrono::time_point<HighResClock>;
+using Duration     = std::chrono::duration<float>;
+
+class Clock {
+public:
+    Clock();
+    ~Clock() = default;
+
+    void Tick();
+
+    void Pause();
+    void Resume();
+    void TogglePause();
+    bool IsPaused() const;
+
+    float GetFps() const;
+    float GetDeltaTime() const;
+    double GetElapsedTime() const;
+
+    float GetTimeScale() const;
+    void SetTimeScale(float timeScale);
+
+    void Reset();
+
+    void SetFpsUpdateInterval(float interval);
+
+private:
+    TimePoint m_currentTime;
+    TimePoint m_lastTime;
+    TimePoint m_startTime;
+
+    float m_deltaTime;
+    float m_rawDeltaTime;
+    double m_totalTime;
+    float m_timeScale;
+
+    unsigned int m_frameCount;
+    float m_fpsUpdateInterval;
+    float m_fpsAccumulator;
+    float m_fps;
+
+    double m_fixedTimestep;
+    double m_fixedTimeAccumulator; // i will do it later
+
+    bool m_isPaused;
+    bool m_useFixedTimestep;
+};
+
+class Profiler {
+public:
+    Profiler()  = default;
+    ~Profiler() = default;
+
+    void Begin(const std::string &label);
+    void End(const std::string &label);
+
+    double GetDuration(const std::string &label);
+
+private:
+    std::unordered_map<std::string, TimePoint> m_beginTimePoint;
+    std::unordered_map<std::string, TimePoint> m_endTimePoint;
+};
+
+} // namespace zui
