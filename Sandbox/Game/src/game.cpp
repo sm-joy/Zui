34b488c02f446683414090aea6ee695be300cd3f
Zihad Joy <smz.joy73@gmail.com>
2026-03-28 14:41:32 +0600

    chore: test new layer module

--- Sandbox/Game/src/game.cpp
+++ Sandbox/Game/src/game.cpp
@@ -1,5 +1,17 @@
 #include <Zui.hpp>
+
+class MainMenuLayer : public zui::Layer {
+    void OnAttach() override {
+        LOGGER_TRACE("Heloo from Main Menu Layer");
+    }
+};
+
+
 class Game : public zui::Application {
+    void OnInit(zui::LayerContext& layerContext) override {
+        LOGGER_TRACE("Hello from application");
+        layerContext.PushLayer<MainMenuLayer>();
+    }
     zui::WinConfig SetWindowConfig() override { return {.Title = "Game", .Width = 500, .Height = 500}; }
 };
 
