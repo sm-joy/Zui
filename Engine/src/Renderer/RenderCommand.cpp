f9c5e6133634d1960795fd30925c14da6442a089
Zihad Joy <smz.joy73@gmail.com>
2026-04-09 16:41:18 +0600

    feat(render-command): the bridge connecting highlevel Renderer with lowlevel RendererAPI

---
+++ Engine/src/Renderer/RenderCommand.cpp
@@ -0,0 +1,39 @@
+#include "RenderCommand.hpp"
+#include "RendererAPI.hpp"
+
+namespace zui {
+
+std::unique_ptr<RendererAPI> RenderCommand::s_rendererAPI = nullptr;
+
+void RenderCommand::Init() {
+    if (!s_rendererAPI) {
+        s_rendererAPI = RendererAPI::Create();
+    }
+    s_rendererAPI->Init();
+}
+
+void RenderCommand::Shutdown() {
+    s_rendererAPI.reset();
+}
+
+void RenderCommand::SetViewport(int x, int y, int w, int h) {
+    if (!s_rendererAPI) return;
+    s_rendererAPI->SetViewport(x, y, w, h);
+}
+
+void RenderCommand::SetClearColor(const Color& c) {
+    if (!s_rendererAPI) return;
+    s_rendererAPI->SetClearColor(c);
+}
+
+void RenderCommand::Clear() {
+    if (!s_rendererAPI) return;
+    s_rendererAPI->Clear();
+}
+
+void RenderCommand::DrawIndexed(const std::shared_ptr<VertexArray>& va, std::uint32_t indexCount) {
+    if (!s_rendererAPI) return;
+    s_rendererAPI->DrawIndexed(va, indexCount);
+}
+
+} // namespace zui
---
+++ Engine/src/Renderer/RenderCommand.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "../Values/Color.hpp"
+#include "RendererAPI.hpp"
+#include "VertexArray.hpp"
+
+#include <memory>
+
+namespace zui {
+
+class RenderCommand {
+public:
+    static void Init();
+    static void Shutdown();
+    static void SetViewport(int x, int y, int w, int h);
+    static void SetClearColor(const Color& c);
+    static void Clear();
+    static void DrawIndexed(const std::shared_ptr<VertexArray>& va, std::uint32_t indexCount = 0);
+
+private:
+    static std::unique_ptr<RendererAPI> s_rendererAPI;
+};
+
+} // namespace zui
