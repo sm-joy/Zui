cd4545fd06e890d1f0e8fa95117b9f029c512cd0
Zihad Joy <smz.joy73@gmail.com>
2026-04-16 23:44:59 +0600

    sandbox: test material, and mesh

--- Sandbox/Game/src/game.cpp
+++ Sandbox/Game/src/game.cpp
@@ -7,29 +7,58 @@
 class MainMenuLayer : public zui::Layer {
 public:
     zui::PerspectiveCameraController CameraController{500.0f / 500.0f};
-    std::shared_ptr<zui::Shader> ShaderProgram;
-    std::shared_ptr<zui::VertexBuffer> VertexBfr;
-    std::shared_ptr<zui::VertexArray> VertexArr;
-    std::shared_ptr<zui::Texture2D> Texture;
+    std::shared_ptr<zui::Material> Material;
+    std::shared_ptr<zui::Mesh> Mesh;
 
-    float Vertices[180] = {
-        -0.5f, -0.5f, -0.5f, 0.0f, 0.0f, 0.5f,  -0.5f, -0.5f, 1.0f, 0.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f,
-        0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 0.0f,
+    std::vector<zui::Vertex> Vertices = {
+        // Front face
+        {{-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f}},
+        {{0.5f, -0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}, {1.0f, 0.0f}},
+        {{0.5f, 0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
+        {{0.5f, 0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
+        {{-0.5f, 0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f}},
+        {{-0.5f, -0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f}},
 
-        -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, 0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 1.0f, -0.5f, 0.5f,  0.5f,  0.0f, 1.0f, -0.5f, -0.5f, 0.5f,  0.0f, 0.0f,
+        // Back face
+        {{-0.5f, -0.5f, 0.5f}, {0.0f, 0.0f, -1.0f}, {0.0f, 0.0f}},
+        {{0.5f, -0.5f, 0.5f}, {0.0f, 0.0f, -1.0f}, {1.0f, 0.0f}},
+        {{0.5f, 0.5f, 0.5f}, {0.0f, 0.0f, -1.0f}, {1.0f, 1.0f}},
+        {{0.5f, 0.5f, 0.5f}, {0.0f, 0.0f, -1.0f}, {1.0f, 1.0f}},
+        {{-0.5f, 0.5f, 0.5f}, {0.0f, 0.0f, -1.0f}, {0.0f, 1.0f}},
+        {{-0.5f, -0.5f, 0.5f}, {0.0f, 0.0f, -1.0f}, {0.0f, 0.0f}},
 
-        -0.5f, 0.5f,  0.5f,  1.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 1.0f, 1.0f, -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  0.5f,  1.0f, 0.0f,
+        // Left face
+        {{-0.5f, 0.5f, 0.5f}, {-1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
+        {{-0.5f, 0.5f, -0.5f}, {-1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},
+        {{-0.5f, -0.5f, -0.5f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+        {{-0.5f, -0.5f, -0.5f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+        {{-0.5f, -0.5f, 0.5f}, {-1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+        {{-0.5f, 0.5f, 0.5f}, {-1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
 
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, 0.5f,  -0.5f, -0.5f, 0.0f, 1.0f,
-        0.5f,  -0.5f, -0.5f, 0.0f, 1.0f, 0.5f,  -0.5f, 0.5f,  0.0f, 0.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+        // Right face
+        {{0.5f, 0.5f, 0.5f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
+        {{0.5f, 0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {1.0f, 1.0f}},
+        {{0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+        {{0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f}},
+        {{0.5f, -0.5f, 0.5f}, {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f}},
+        {{0.5f, 0.5f, 0.5f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
 
-        -0.5f, -0.5f, -0.5f, 0.0f, 1.0f, 0.5f,  -0.5f, -0.5f, 1.0f, 1.0f, 0.5f,  -0.5f, 0.5f,  1.0f, 0.0f,
-        0.5f,  -0.5f, 0.5f,  1.0f, 0.0f, -0.5f, -0.5f, 0.5f,  0.0f, 0.0f, -0.5f, -0.5f, -0.5f, 0.0f, 1.0f,
+        // Top face
+        {{-0.5f, 0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},
+        {{0.5f, 0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {1.0f, 1.0f}},
+        {{0.5f, 0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
+        {{0.5f, 0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f}},
+        {{-0.5f, 0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f}},
+        {{-0.5f, 0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},
 
-        -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f, 0.5f,  0.5f,  -0.5f, 1.0f, 1.0f, 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
-        0.5f,  0.5f,  0.5f,  1.0f, 0.0f, -0.5f, 0.5f,  0.5f,  0.0f, 0.0f, -0.5f, 0.5f,  -0.5f, 0.0f, 1.0f};
+        // Bottom face
+        {{-0.5f, -0.5f, -0.5f}, {0.0f, -1.0f, 0.0f}, {0.0f, 1.0f}},
+        {{0.5f, -0.5f, -0.5f}, {0.0f, -1.0f, 0.0f}, {1.0f, 1.0f}},
+        {{0.5f, -0.5f, 0.5f}, {0.0f, -1.0f, 0.0f}, {1.0f, 0.0f}},
+        {{0.5f, -0.5f, 0.5f}, {0.0f, -1.0f, 0.0f}, {1.0f, 0.0f}},
+        {{-0.5f, -0.5f, 0.5f}, {0.0f, -1.0f, 0.0f}, {0.0f, 0.0f}},
+        {{-0.5f, -0.5f, -0.5f}, {0.0f, -1.0f, 0.0f}, {0.0f, 1.0f}},
+    };
 
     glm::vec3 CubePositions[50] = {
         {0.0f, 0.0f, 0.0f},    {2.0f, 5.0f, -15.0f},   {-1.5f, -2.2f, -2.5f},  {-3.8f, -2.0f, -12.3f},
@@ -46,18 +75,11 @@ public:
         {0.0f, 0.0f, -25.0f},  {5.0f, 3.0f, -22.0f},   {8.0f, -1.0f, -20.0f}};
 
     void OnAttach() override {
-
         zui::Renderer::SetClearColor(zui::colors::DARK_GRAY);
 
-        Texture = zui::Texture2D::Create("Sandbox/assets/dd.jpeg");
-
-        VertexArr = zui::VertexArray::Create();
-        VertexBfr = zui::VertexBuffer::Create(Vertices, sizeof(Vertices));
-        VertexBfr->SetLayout({{zui::ShaderDataType::FLOAT3, "aPos"}, {zui::ShaderDataType::FLOAT2, "aTexCoord"}});
-
-        ShaderProgram = zui::Shader::CreateFromFile("Cube", "Sandbox/assets/basic_shader.glsl");
-
-        VertexArr->AddVertexBuffer(VertexBfr);
+        Mesh = zui::Mesh::Create(Vertices);
+        Material = zui::Material::Create(zui::Shader::CreateFromFile("Cube", "Sandbox/assets/basic_shader.glsl"));
+        Material->SetTexture("u_Texture", zui::Texture2D::Create("Sandbox/assets/dd.jpeg"));
     }
 
     void OnUpdate(zui::FrameContext& ctx, float dt) override { CameraController.OnUpdate(dt); }
@@ -72,19 +94,14 @@ public:
             float angle = 20.0f * static_cast<float>(i);
             model = glm::rotate(model, (float)(zui::GClock::GetElapsedTime()), glm::vec3(0.5f, 1.0f, 0.0f));
 
-            Texture->Bind(0);
-            ShaderProgram->Bind();
-            ShaderProgram->Set("u_Texture", 0);
-            zui::Renderer::Submit(ShaderProgram, VertexArr, model);
+            zui::Renderer::Submit(Mesh, Material, model);
         }
         zui::Renderer::EndScene();
     }
 };
 
 class Game : public zui::Application {
-    void OnInit(zui::FrameContext& ctx) override {
-        ctx.Layer.PushLayer<MainMenuLayer>();
-    }
+    void OnInit(zui::FrameContext& ctx) override { ctx.Layer.PushLayer<MainMenuLayer>(); }
     zui::WinConfig SetWindowConfig() override {
         return {.Title = "Game", .Width = 500, .Height = 500, .Resizable = true};
     }
--- Sandbox/assets/basic_shader.glsl
+++ Sandbox/assets/basic_shader.glsl
@@ -1,7 +1,8 @@
 #type vertext
 #version 330 core
-layout (location = 0) in vec3 aPos;
-layout(location = 1) in vec2 aTexCoord;
+layout (location = 0) in vec3 a_Position;
+layout (location = 1) in vec3 a_Normal;
+layout(location = 2) in vec2 a_TexCoord;
 
 uniform mat4 u_Model;
 uniform mat4 u_ViewProjection;
@@ -10,8 +11,8 @@ out vec2 v_TexCoord;
 
 void main()
 {
-    v_TexCoord = aTexCoord;
-    gl_Position = u_ViewProjection * u_Model * vec4(aPos, 1.0f);
+    v_TexCoord = a_TexCoord;
+    gl_Position = u_ViewProjection * u_Model * vec4(a_Position, 1.0f);
 }
 
 #type fragment
